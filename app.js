87078a44d6fc37faa5fb147e84dc6d74f02bd79f
Allen Clayton <ajclayton@gmail.com>
2016-04-14 21:54:16 -0600

    Added file upload

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 var RouteHandler = require('./lib/dynamicRouteHandler.js'),
 	routeHandler = new RouteHandler('../routesExample/routes.json');
 const Hapi = require('hapi');
-
+const fs = require('fs');
 const server = new Hapi.Server();
 
 var config = require('./config.json');
@@ -25,6 +25,44 @@ server.route({
 	method: 'GET',
 	path: '/admin/reload',
 	handler: routeHandler.reloadFromFile
+});
+server.route({
+	method: 'POST',
+	path: '/admin/upload/{filename}',
+	config: {
+		payload: {
+			output: 'stream',
+			parse: true,
+			allow: 'multipart/form-data'
+		},
+		handler: function (request, reply) {
+			var data = request.payload;
+			if (data.file) {
+				//var name = data.file.hapi.filename;
+				var name = request.params.filename;
+				var path = __dirname + "/routesExample/" + name;
+				var file = fs.createWriteStream(path);
+
+				file.on('error', function (err) {
+					console.error(err);
+					reply("Sorry, an error occurred.")
+				});
+
+				data.file.pipe(file);
+
+				data.file.on('end', function (err) {
+					/*var ret = {
+						filename: data.file.hapi.filename,
+						headers: data.file.hapi.headers
+					};
+					reply(JSON.stringify(ret));*/
+					reply("Successfully saved the file: " + name)
+				})
+			}else{
+				reply("Could not save file. Did you send it?")
+			}
+		}
+	}
 });
 server.route({
 	method: '*',
